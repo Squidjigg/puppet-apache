b1e31d618de0e68b842cfbe024c5882cb519e602
Squidjigg <squidjigg80@outlook.com>
2025-02-28 12:00:35 +1100

    feat: add 'config' class

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,12 @@
 # @example
 #   include apache
 class apache (
- String $install_name,
+  String $install_name,
+  Array $config_paths,
 ) {
- include apache::install
+  contain apache::install
+  contain apache::config
+
+  Class['::apache::install']
+  -> Class['::apache::config']
 }
