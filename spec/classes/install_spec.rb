64a5aee698f1ba968296dd13865fb7eaf2cd8475
Squidjigg <squidjigg80@outlook.com>
2025-03-28 11:32:25 +1100

    feat: add basic spec tests

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -7,7 +7,7 @@ describe 'apache::install' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile.with_all_deps }
+      it { is_expected.not_to compile.with_all_deps }
     end
   end
 end
